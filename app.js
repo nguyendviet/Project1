87da623362cfab88cb6793478017e683b50193ef
Viet Nguyen <vietjul@gmail.com>
2017-09-23 18:03:53 -0400

    create game

--- app.js
+++ app.js
@@ -10,13 +10,69 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var ref = database.ref();
 
 var food = ['pizza', 'hamburger', 'pho', 'carbonara'];
+var myInfo = {name: '', gameID: ''};
 var foodChosen = food[Math.floor(Math.random() * food.length)];
 
 console.log('food chosen: ', foodChosen);
 
+function enterName() {
+	var name = $('.name').val().trim();
 
+	if (name) {
+		console.log('name: ', name);
+
+		myInfo.name = name;
+
+		console.log('myInfo: ', myInfo);
+
+		$('.userLogin').html('Hi ' + name + '!');
+		$('.choice').css('display', 'block');
+	}
+	else {
+		return;
+	}
+}
+
+ref.on('child_added', function(snap) {
+	myInfo.gameID = snap.key;
+
+	console.log('game ID: ', snap.key);
+	console.log('myInfo: ', myInfo);
+});
+
+$('.btnEnter').on('click', function(event) {
+	event.preventDefault();
+
+	enterName();
+});
+
+$('.btnCreate').on('click', function(event) {
+	event.preventDefault();
+
+	database.ref().push({Player1: myInfo.name});
+
+	console.log('push to firebase: ', myInfo.name);
+
+	$('.choice').hide();
+	$('.game').css('display', 'block');
+	$('.gameID').html('Your game ID: ' + myInfo.gameID)
+});
+
+$('.btnJoin').on('click', function(event) {
+	event.preventDefault();
+
+	var name = $('.gameID').val().trim();
+
+	if (name) {
+		console.log('existing game ID: ', name);
+	}
+	else {
+		return;
+	}
+});
 
 //if create game, copy key
 
